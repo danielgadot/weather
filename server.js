a4ab8a39fc7dfc63fadf97e3febad057af9b7c91
danielgadot <danielgadot@gmail.com>
2020-09-10 12:11:39 +0300

    css, server

--- server.js
+++ server.js
@@ -1,18 +1,16 @@
 const express = require('express');
-const app = express();
-const port = 3000
+// const favicon = require('express-favicon');
 const path = require('path');
-const bodyParser = require('body-parser');
-var cors = require('cors');
-
-app.use(cors());
-app.options('*', cors());
-
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(bodyParser.json());
-app.get('/*',(req, res) => {
-  res.sendFile(path.join(__dirname + `/dist/weather/index.html`))
+const port = process.env.PORT || 8080;
+const app = express();
+// app.use(favicon(__dirname + '/build/favicon.ico'));
+// the __dirname is the current directory from where the script is running
+app.use(express.static(__dirname));
+app.use(express.static(path.join(__dirname, 'build')));
+app.get('/ping', function (req, res) {
+  return res.send('pong');
 });
-
-
-app.listen(port, () => console.log(`Example app listening on port ${port}!`))
+app.get('/*', function (req, res) {
+  res.sendFile(path.join(__dirname, 'build', 'index.html'));
+});
+app.listen(port);
